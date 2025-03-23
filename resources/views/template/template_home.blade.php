792570819260e5d56064fd27206a96e10e2ece1e
Fakhry Linux <fakhry.linux@gmail.com>
2025-03-24 00:36:18 +0800

    Change asset to use secure_asset

--- resources/views/template/template_home.blade.php
+++ resources/views/template/template_home.blade.php
@@ -20,10 +20,10 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
 
     <!-- Libraries Stylesheet -->
-    <link href="{{asset('assets/lib/owlcarousel/assets/owl.carousel.min.css')}}" rel="stylesheet" />
+    <link href="{{ secure_asset('assets/lib/owlcarousel/assets/owl.carousel.min.css')}}" rel="stylesheet" />
 
     <!-- Customized Bootstrap Stylesheet -->
-    <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet" />
+    <link href="{{ secure_asset('assets/css/style.css') }}" rel="stylesheet" />
 
     @yield('head')
 </head>
